0039d685fb4e017769bcded1b7f2c7dd8768769d
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2023-12-06 19:55:41 +0530

    working on side bar

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -19,82 +19,88 @@ const SideNav = () => {
   };
 
   return (
-    <div className="m-2">
-      <ul className="list-none p-0 m-4">
-        <li
-          id="home"
-          className={`flex justify-between items-center mb-4 p-4 ${
-            openIcon === "Home"
-              ? "bg-gray-800 border-2 rounded-full border-gray-500"
-              : ""
-          }`}
-          onMouseEnter={() => handleIconClick("Home")}
-          onMouseLeave={handleIconLeave}
-        >
-          <a href="">
-            <FiHome />
-          </a>
-          {openIcon === "Home" && <span className="text-sm ml-2">Home</span>}
-        </li>
-        <li
-          id="about"
-          className={`flex justify-between items-center mb-4 p-4 ${
-            openIcon === "About"
-              ? "bg-gray-800 border-2 rounded-full border-gray-500"
-              : ""
-          }`}
-          onMouseEnter={() => handleIconClick("About")}
-          onMouseLeave={handleIconLeave}
-        >
-          <FaRegFaceGrin />
-          {openIcon === "About" && <span className="text-sm ml-2">About</span>}
-        </li>
-        <li
-          id="resume"
-          className={`flex justify-between items-center mb-4 p-4 ${
-            openIcon === "Resume"
-              ? "bg-gray-800 border-2 rounded-full border-gray-500"
-              : ""
-          }`}
-          onMouseEnter={() => handleIconClick("Resume")}
-          onMouseLeave={handleIconLeave}
-        >
-          <MdOutlineContactPage />
-          {openIcon === "Resume" && (
-            <span className="text-sm ml-2">Resume</span>
-          )}
-        </li>
-        <li
-          id="portfolio"
-          className={`flex justify-between items-center mb-4 p-4 ${
-            openIcon === "Portfolio"
-              ? "bg-gray-800 border-2 rounded-full border-gray-500"
-              : ""
-          }`}
-          onMouseEnter={() => handleIconClick("Portfolio")}
-          onMouseLeave={handleIconLeave}
-        >
-          <MdOutlineHomeRepairService />
-          {openIcon === "Portfolio" && (
-            <span className="text-sm ml-2">Portfolio</span>
-          )}
-        </li>
-        <li
-          id="contact"
-          className={`flex justify-between items-center mb-4 p-4 ${
-            openIcon === "Contact"
-              ? "bg-gray-800 border-2 rounded-full border-gray-500"
-              : ""
-          }`}
-          onMouseEnter={() => handleIconClick("Contact")}
-          onMouseLeave={handleIconLeave}
-        >
-          <MdOutlineMailOutline />
-          {openIcon === "Contact" && (
-            <span className="text-sm ml-2">Contact</span>
-          )}
-        </li>
-      </ul>
+    <div className="absolute top-20 ">
+      <div
+        className={`m-2 transition-all duration-500 ${openIcon ? "fixed" : ""}`}
+      >
+        <div className="list-none p-0 m-4">
+          <div
+            id="home"
+            className={`flex justify-between items-center mb-4 p-4 ${
+              openIcon === "Home"
+                ? "bg-gray-800 border-2 rounded-full border-gray-500"
+                : ""
+            }`}
+            onMouseEnter={() => handleIconClick("Home")}
+            onMouseLeave={handleIconLeave}
+          >
+            <a href="">
+              <FiHome />
+            </a>
+            {openIcon === "Home" && <span className="text-sm ml-2">Home</span>}
+          </div>
+          <div
+            id="about"
+            className={`flex justify-between items-center mb-4 p-4 ${
+              openIcon === "About"
+                ? "bg-gray-800 border-2 rounded-full border-gray-500"
+                : ""
+            }`}
+            onMouseEnter={() => handleIconClick("About")}
+            onMouseLeave={handleIconLeave}
+          >
+            <FaRegFaceGrin />
+            {openIcon === "About" && (
+              <span className="text-sm ml-2">About</span>
+            )}
+          </div>
+          <div
+            id="resume"
+            className={`flex justify-between items-center mb-4 p-4 ${
+              openIcon === "Resume"
+                ? "bg-gray-800 border-2 rounded-full border-gray-500"
+                : ""
+            }`}
+            onMouseEnter={() => handleIconClick("Resume")}
+            onMouseLeave={handleIconLeave}
+          >
+            <MdOutlineContactPage />
+            {openIcon === "Resume" && (
+              <span className="text-sm ml-2">Resume</span>
+            )}
+          </div>
+          <div
+            id="portfolio"
+            className={`flex justify-between items-center mb-4 p-4 ${
+              openIcon === "Portfolio"
+                ? "bg-gray-800 border-2 rounded-full border-gray-500"
+                : ""
+            }`}
+            onMouseEnter={() => handleIconClick("Portfolio")}
+            onMouseLeave={handleIconLeave}
+          >
+            <MdOutlineHomeRepairService />
+            {openIcon === "Portfolio" && (
+              <span className="text-sm ml-2">Portfolio</span>
+            )}
+          </div>
+          <div
+            id="contact"
+            className={`flex justify-between items-center mb-4 p-4 ${
+              openIcon === "Contact"
+                ? "bg-gray-800 border-2 rounded-full border-gray-500"
+                : ""
+            }`}
+            onMouseEnter={() => handleIconClick("Contact")}
+            onMouseLeave={handleIconLeave}
+          >
+            <MdOutlineMailOutline />
+            {openIcon === "Contact" && (
+              <span className="text-sm ml-2">Contact</span>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
