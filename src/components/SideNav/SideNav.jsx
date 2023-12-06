e489ba49fff8a66c13bc8bf0d8cd751a99a73f30
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2023-12-07 00:18:23 +0530

    sidebar complete

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -7,9 +7,10 @@ import {
 import { FiHome } from "react-icons/fi";
 import { FaRegFaceGrin } from "react-icons/fa6";
 import { GiHamburger } from "react-icons/gi";
+import { RiDeleteBin5Fill } from "react-icons/ri";
 
 const Sidebar = () => {
-  const [open, setOpen] = useState(null);
+  const [open, setOpen] = useState(false);
 
   const handleClick = () => {
     setOpen(!open);
@@ -17,36 +18,39 @@ const Sidebar = () => {
 
   return (
     <div className="absolute h-screen">
-      <div className="w-20 bg-gray-800 fixed top-0 left-0 bottom-0 z-9997 overflow-y-auto">
-        <div className="p-4" onClick={handleClick}>
-          <GiHamburger />
+      <div className="w-20 fixed top-0 bottom-0 z-9 transition-all duration-100 ease-in-out">
+        <div className="p-4 cursor-pointer " onClick={handleClick}>
+          {open ? (
+            <RiDeleteBin5Fill className="ml-3 text-xl" />
+          ) : (
+            <GiHamburger className="ml-3 text-xl" />
+          )}
         </div>
-        <ul className="top-20 mt-20">
+        <ul
+          className={`top-20 mt-20 cursor-pointer ${open ? "block" : "hidden"}`}
+        >
           <li className="p-4 hover:bg-gray-600">
-            <FiHome className="ml-3" />
+            <FiHome className="ml-3 text-xl" />
             <span>Home</span>
           </li>
           <li className="p-4 hover:bg-gray-600">
-            <FaRegFaceGrin className="ml-3" />
+            <FaRegFaceGrin className="ml-3 text-xl" />
             <span>About</span>
           </li>
           <li className="p-4 hover:bg-gray-600">
-            <MdOutlineContactPage className="ml-3" />
+            <MdOutlineContactPage className="ml-3 text-xl" />
             <span>Skills</span>
           </li>
           <li className="p-4 hover:bg-gray-600">
-            <MdOutlineHomeRepairService className="ml-3" />
+            <MdOutlineHomeRepairService className="ml-3 text-xl" />
             <span>Work</span>
           </li>
           <li className="p-4 hover:bg-gray-600">
-            <MdOutlineMailOutline className="ml-3" />
+            <MdOutlineMailOutline className="ml-3 text-xl" />
             <span>Contact</span>
           </li>
         </ul>
       </div>
-      {/* <div className="w-3/4 bg-gray-100">
-        <p>{hoveredItem}</p>
-      </div> */}
     </div>
   );
 };
