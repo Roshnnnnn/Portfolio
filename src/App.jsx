d69919ec488d1e0156ccde1feae32e61584be968
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2023-12-07 00:48:41 +0530

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import "./App.css";
-import About from "./components/About/About";
-import Contact from "./components/Contact/Contact";
-import SideNav from "./components/SideNav/SideNav";
-import Header from "./components/Header/Header";
+// import "./App.css";
+// import {
+//   createBrowserRouter,
+//   // RouterProvider,
+//   Route,
+// } from "react-router-dom/cjs/react-router-dom";
+// import About from "./components/About/About.jsx";
+// import Contact from "./components/Contact/Contact.jsx";
+// import SideNav from "./components/SideNav/SideNav.jsx";
+// import Header from "./components/Header/Header.jsx";
 
-function App() {
-  return (
-    <>
-      <Header />
-      <About />
-      <Contact />
-      <SideNav />
-    </>
-  );
-}
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <SideNav />
+//       <Routes>
+//         <Route path="/" element={<Header />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/contact" element={<Contact />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
 
-export default App;
+// export default App;
