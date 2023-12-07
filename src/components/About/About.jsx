f809e056c005f7a6dda6be9099c8a6950c43422b
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2023-12-07 20:35:10 +0530

    about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,51 +1,61 @@
-// export default function About() {
-//   return (
-//     <div className="py-16 bg-white">
-//       <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
-//         <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
-//           <div className="md:5/12 lg:w-5/12">
-//             <img
-//               src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
-//               alt="image"
-//             />
-//           </div>
-//           <div className="md:7/12 lg:w-6/12">
-//             <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
-//               React development is carried out by passionate developers
-//             </h2>
-//             <p className="mt-6 text-gray-600">
-//               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum
-//               omnis voluptatem accusantium nemo perspiciatis delectus atque
-//               autem! Voluptatum tenetur beatae unde aperiam, repellat expedita
-//               consequatur! Officiis id consequatur atque doloremque!
-//             </p>
-//             <p className="mt-4 text-gray-600">
-//               Nobis minus voluptatibus pariatur dignissimos libero quaerat iure
-//               expedita at? Asperiores nemo possimus nesciunt dicta veniam
-//               aspernatur quam mollitia.
-//             </p>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-import React from "react";
+import { FaArrowRight } from "react-icons/fa";
 
 const About = () => {
   return (
-    <div className="text-center justify-center mt-5">
-      <h2 className="text-2xl font-bold uppercase relative text-gray-700 ">
-        About
-      </h2>
-      <p className="items-center w-[65rem] ml-[10rem]">
-        Hello, my name is Roshan Kumar Yadav. I completed my BCA in 2021 from
-        Vikrant Group of Institutions, which is affiliated with Jiwaji
-        University. After that, I joined a BPO job at Jio. During that time, I
-        realized that I wasn't finding fulfillment in my work and decided to
-        consider a change in my career path.
-      </p>
+    <div className="ml-[10rem]">
+      <div className="text-center pb-30">
+        <h2 className="text-2xl font-bold uppercase mb-5 pb-5 relative text-gray-700">
+          About
+        </h2>
+        <p className="mt-6 text-gray-600 mr-[5rem] ">
+          Hello, my name is Roshan Kumar Yadav. I completed my BCA in 2021 from
+          Vikrant Group of Institutions, which is affiliated with Jiwaji
+          University. After that, I joined a BPO job at Jio. During that time, I
+          realized that I wasn't finding fulfillment in my work and decided to
+          consider a change in my career path.
+        </p>
+      </div>
+      <div className="flex items-center gap-x-6 mt-5">
+        <div className="flex-shrink-0 w-1/3">
+          <img
+            src="https://bootstrapmade.com/demo/templates/MyResume/assets/img/profile-img.jpg"
+            alt="image"
+          />
+        </div>
+        <div className="">
+          <h3 className="font-bold text-2xl text-gray-600 mb-2">
+            Frontend Engineer
+          </h3>
+          <p className="font-bold text-sm text-gray-700">
+            Hello, my name is Roshan Kumar Yadav. I completed my BCA in 2021
+            from Vikrant Group of Institutions, which is affiliated with Jiwaji
+            University. After that, I joined a BPO job at Jio. During that time,
+            I realized that I wasn't finding fulfillment in my work and decided
+            to consider a change in my career path.
+          </p>
+          <div className="flex flex-wrap gap-x-6 mt-0 -mx-3">
+            <div className="flex-shrink-0 w-1/2">
+              <ul className="list-none p-0">
+                <li className="mb-5 flex items-center">
+                  <FaArrowRight className="text-blue-600 text-sm mr-1 leading-3" />
+                  <strong className="mr-5">Graduation:</strong>
+                  <span>BCA</span>
+                </li>
+                <li className="mb-5 flex items-center">
+                  <FaArrowRight className="text-blue-600 text-sm mr-1 leading-3" />
+                  <strong className="mr-5">Graduation:</strong>
+                  <span>BCA</span>
+                </li>
+                <li className="mb-5 flex items-center">
+                  <FaArrowRight className="text-blue-600 text-sm mr-1 leading-3" />
+                  <strong className="mr-5">Graduation:</strong>
+                  <span>BCA</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
