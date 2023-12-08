b52c2fe3f1230520e94c8af693f7dd337789972f
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2023-12-09 00:13:12 +0530

    1st time

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
+import SideNav from "./components/SideNav/SideNav.jsx";
+import Header from "./components/Header/Header.jsx";
 
 const Layout = () => {
   return (
     <>
+      <SideNav />
+      <Header />
       <Outlet />
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
 import "./index.css";
 import {
   Route,
@@ -9,18 +8,15 @@ import {
   createRoutesFromElements,
 } from "react-router-dom";
 import Layout from "./Layout.jsx";
-import SideNav from "./components/SideNav/SideNav.jsx";
-import Header from "./components/Header/Header.jsx";
+
 import About from "./components/About/About.jsx";
 import Contact from "./components/Contact/Contact.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-      <Route path="" element={<SideNav />} />
-      <Route path="" element={<Header />} />
-      <Route path="" element={<About />} />
-      <Route path="" element={<Contact />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
     </Route>
   )
 );
