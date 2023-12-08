13e3c34e8366c2fb38ac0a2e5920eaf19ba857f3
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2023-12-08 23:53:08 +0530

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,3 @@
-// import "./App.css";
-// import {
-//   createBrowserRouter,
-//   // RouterProvider,
-//   Route,
-// } from "react-router-dom/cjs/react-router-dom";
-// import About from "./components/About/About.jsx";
-// import Contact from "./components/Contact/Contact.jsx";
-// import SideNav from "./components/SideNav/SideNav.jsx";
-// import Header from "./components/Header/Header.jsx";
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <SideNav />
-//       <Routes>
-//         <Route path="/" element={<Header />} />
-//         <Route path="/about" element={<About />} />
-//         <Route path="/contact" element={<Contact />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-
-// import React from 'react'
 // import Header from "./components/Header/Header.jsx";
 import SideNav from "./components/SideNav/SideNav.jsx";
 // import About from "./components/About/About.jsx";
--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -10,28 +10,6 @@ import { GiHamburger } from "react-icons/gi";
 import { RiDeleteBin5Fill } from "react-icons/ri";
 
 const Sidebar = () => {
-  let Links = [
-    {
-      name: "Home",
-      link: "/",
-    },
-    {
-      name: "About",
-      link: "/about",
-    },
-    {
-      name: "Skills",
-      link: "/skills",
-    },
-    {
-      name: "Work",
-      link: "/work",
-    },
-    {
-      name: "Contact",
-      link: "/contact",
-    },
-  ];
   const [open, setOpen] = useState(false);
 
   const handleClick = () => {
@@ -40,7 +18,11 @@ const Sidebar = () => {
 
   return (
     <div className="absolute h-screen">
-      <div className="w-20 fixed top-0 bottom-0 z-9 transition-all duration-100 ease-in-out">
+      <div
+        className={`w-20 fixed top-0 ${
+          open ? "lg:right-0" : "sm:left-0"
+        } z-9 transition-all duration-100 ease-in-out`}
+      >
         <div className="p-4 cursor-pointer" onClick={handleClick}>
           {open ? (
             <RiDeleteBin5Fill className="ml-3 text-xl" />
@@ -73,15 +55,6 @@ const Sidebar = () => {
             <MdOutlineMailOutline className="ml-3 text-xl" />
             <span>Contact</span>
           </li>
-        </ul>
-      </div>
-      <div>
-        <ul className="md:pb-0 pb-12 absolute bg-white">
-          {Links.map((item, index) => (
-            <li className="md:my-0 my-7" key={index}>
-              {item.name}
-            </li>
-          ))}
         </ul>
       </div>
     </div>
