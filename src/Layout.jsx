ea6cdb210f9e0fee6a114128824153917dd67f1e
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2023-12-10 00:13:54 +0530

    portfolio

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 // import Header from "./components/Header/Header.jsx";
 // import Skills from "./components/Skills/SkillPage.jsx";
 import Portfolio from "./components/Portfolio/Portfolio.jsx";
+import Projects from "./components/Portfolio/Projects.jsx";
 
 const Layout = () => {
   return (
@@ -10,7 +11,8 @@ const Layout = () => {
       {/* <SideNav /> */}
       {/* <Header /> */}
       {/* <Skills /> */}
-      <Portfolio />
+      {/* <Portfolio /> */}
+      <Projects />
       <Outlet />
     </>
   );
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,27 +1,67 @@
-const Portfolio = () => {
-  const work = [
-    { name: "Project1" },
-    { name: "Project2" },
-    { name: "project3" },
-  ];
+import { useState } from "react";
 
+const Work = () => {
   return (
-    <div className="mx-4 sm:mx-10 md:mx-20 lg:mx-40 xl:mx-60">
-      <h1 className="text-2xl font-bold mb-6 sm:mb-10">Projects</h1>
-      <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-2">
-        {work.map((works, index) => (
-          <div
-            key={index}
-            className={`h-40 lg:h-56 xl:h-72 bg-black text-yellow-50 p-4 ${
-              index === work.length - 1 ? "lg:col-span-2 xl:col-span-2" : ""
-            }`}
-          >
-            {works.name}
-          </div>
-        ))}
+    <div className=" items-center justify-center min-h-screen">
+      <div>
+        <Portfolio
+          image={
+            "https://images.pexels.com/photos/238118/pexels-photo-238118.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          }
+          title={"Ecommerce"}
+        />
+        <Portfolio
+          image={
+            "https://images.pexels.com/photos/238118/pexels-photo-238118.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          }
+          title={"Ecommerce"}
+        />
       </div>
     </div>
   );
 };
 
-export default Portfolio;
+const Portfolio = ({ image, title }) => {
+  const [hover, setHover] = useState(false);
+  return (
+    <div className="h-[25rem] bg-black m-4">
+      <div className="border border-gray-300 p-4 flex h-full w-full">
+        {/* Box 1 - Image Box */}
+        <div
+          className="relative flex-1 bg-gray-200 p-4 m-4 overflow-hidden"
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+        >
+          <img
+            src={image}
+            alt="Image"
+            className="max-w-full max-h-full object-cover hover:transform hover:scale-105 transition-transform"
+          />
+          {hover && (
+            <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 backdrop-filter backdrop-blur-sm text-white">
+              <div className="text-lg font-semibold mb-4">{title}</div>
+              <div className="flex space-x-4 font-bold">
+                <button className="bg-black px-4 py-2 text-white rounded">
+                  Github
+                </button>
+                <button className="px-4 py-2 text-black bg-white rounded">
+                  Visit
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+
+        {/* Box 2 - Text Box */}
+        <div className="flex-1 m-4 border-2 border-solid border-white p-4 text-yellow-100 justify-center">
+          <h2 className="text-xl font-bold mb-2 text-center">{title}</h2>
+          <ul>
+            <li>text should be here</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
